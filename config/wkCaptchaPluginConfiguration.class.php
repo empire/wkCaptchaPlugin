8a06be2234ca4e7fd77fb926f06d3c2d9822c307
Hossein Zolfi <hossein.zolfi@gmail.com>
2011-11-23 16:28:29 +0330

    Initial version

---
+++ config/wkCaptchaPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * wkCaptchaPlugin configuration.
+ * 
+ * @package     wkCaptchaPlugin
+ * @subpackage  config
+ * @author      Hossein Zolfi (hossein.zolfi@gmail.com, empire@cyber-experts.com), Alireza Kazemi (alireza.kazemi@gmail.com, kazemi@cse.shirazu.ac.ir.com), Saeed Kazemi (kazemi.ms@gmail.com, skazemi@cse.shirazu.ac.ir.com)
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class wkCaptchaPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
---
+++ test/bootstrap/functional.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!isset($app))
+{
+  $app = 'frontend';
+}
+
+require_once $_SERVER['SYMFONY'].'/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+function wkCaptchaPlugin_cleanup()
+{
+  sfToolkit::clearDirectory(dirname(__FILE__).'/../fixtures/project/cache');
+  sfToolkit::clearDirectory(dirname(__FILE__).'/../fixtures/project/log');
+}
+wkCaptchaPlugin_cleanup();
+register_shutdown_function('wkCaptchaPlugin_cleanup');
+
+require_once dirname(__FILE__).'/../fixtures/project/config/ProjectConfiguration.class.php';
+$configuration = ProjectConfiguration::getApplicationConfiguration($app, 'test', isset($debug) ? $debug : true);
+sfContext::createInstance($configuration);
